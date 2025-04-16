ac9de1e8034aad2fa68d5539bd3ebc269d6d8f27
Wabryan <Wabryan24@gmail.com>
2025-04-16 07:22:10 +0200

    Supprime des fichiers obsolètes et ajoute les nouveaux fichiers

---
+++ Day-1/exo/first_step/main.js
@@ -0,0 +1,8 @@
+// main.js
+
+const { sum, diff, prod, quot } = require('./math');
+
+console.log("Somme : ", sum(10, 5));
+console.log("Différence : ", diff(10, 5));
+console.log("Produit : ", prod(10, 5));
+console.log("Quotient : ", quot(10, 5));
